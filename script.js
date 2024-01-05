a5bbc9782805d75f8c61fbba8de56dece4c7de50
Aravindakri <155757038+Aravindakri@users.noreply.github.com>
2024-01-05 21:37:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function scrolltoskill(){
+    const skill = document.querySelector('.container-2');
+    skill.scrollIntoView({behavior: "smooth"});
+}
+document.querySelector('.button-1').onclick = scrolltoskill;
+
+function scrolltoskilll(){
+    const skill = document.querySelector('.container-3');
+    skill.scrollIntoView({behavior: "smooth"});
+}
+document.querySelector('.button-2').onclick = scrolltoskill;
+
+function scrolltoskillll(){
+    const skill = document.querySelector('.container-5');
+    skill.scrollIntoView({behavior: "smooth"});
+}
+document.querySelector('.button-3').onclick = scrolltoskill;
+
+
+
